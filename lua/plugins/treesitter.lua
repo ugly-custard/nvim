19990dd49c13a52b3395819e778feed15a649b65
ugly-custard <himanshu.mishra1123@gmail.com>
2026-04-19 16:04:41 +0530

    fix: switch to nvim-treesitter main branch with new API

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,37 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
-    opts = {
-      ensure_installed = {
+    config = function()
+      -- ensure parsers are installed
+      local langs = {
         "bash",
         "c",
         "diff",
         "vim",
         "lua",
         "markdown",
+        "markdown_inline",
         "javascript",
         "typescript",
         "css",
         "html",
         "json",
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { "ruby" },
-      },
-      indent = { enable = true, disable = { "ruby" } },
-      query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = { "BufWrite", "CursorHold" },
-      },
-    },
-    config = function()
-      require("nvim-treesitter.install").prefer_git = true
+      }
+      -- Install missing parsers on startup
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          local installed = require("nvim-treesitter.config").get_installed()
+          local to_install = vim.tbl_filter(function(lang)
+            return not vim.list_contains(installed, lang)
+          end, langs)
+          if #to_install > 0 then
+            require("nvim-treesitter").install(to_install)
+          end
+        end,
+        once = true,
+      })
     end,
   },
 }
