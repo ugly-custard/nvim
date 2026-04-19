5b711324ece4221022dd3437dbf5f6ae69670a73
ugly-custard <himanshu.mishra1123@gmail.com>
2026-04-19 16:06:29 +0530

    refactor: replace Comment.nvim with built-in gc/gcc + ts-context-commentstring

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,12 +1,17 @@
+-- Comment.nvim removed: Neovim 0.12 has built-in gc/gcc commenting.
+-- nvim-ts-context-commentstring is kept for JSX/TSX support via built-in commenting.
 return {
-  "numToStr/Comment.nvim",
-  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-  config = function()
-    require("ts_context_commentstring").setup({
-      enable_autocmd = false,
-    })
-    require("Comment").setup({
-      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-    })
+  "JoosepAlviste/nvim-ts-context-commentstring",
+  lazy = true,
+  opts = {
+    enable_autocmd = false,
+  },
+  init = function()
+    -- Override the built-in get_option to use ts-context-commentstring
+    local get_option = vim.filetype.get_option
+    vim.filetype.get_option = function(filetype, option)
+      return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
+        or get_option(filetype, option)
+    end
   end,
 }
